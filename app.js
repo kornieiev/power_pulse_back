461367bacf23498358cb759772f575a7164e41b3
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-03-07 10:48:50 +0100

    fix routers links

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-
 const express = require("express"); // создали веб-сервер
 const morgan = require("morgan"); // для логирования HTTP-запросов
 const cors = require("cors"); // позволяет браузеру разрешать кросс-доменные запросы
 const mongoose = require("mongoose"); // создает подключение к базе данных MongoDB
 const usersRoutes = require("./routes/usersRoutes");
 const diaryRouters = require("./routes/diaryRoutes");
-const exerciseRoutes = require("./routes/exerciseRoutes");
-const nutritionsRouter = require("./routes/nutritionsRouter");
 const cloudinaryFn = require("./utils/cloudinaryFn");
 require("dotenv").config(); // ищет в проекте файл .env и читает из него указанные в нем КЛЮЧ=значение
 require("colors"); // для подсвечивания информации выводимой в консоли
 
-const productsRouter = require('./routes/productsRouter')
-
+const productsRouter = require("./routes/productsRouter");
+const exerciseRouters = require("./routes/exercisesRoutes");
 
 const {
-	DB_ADMIN_NAME,
-	DB_ADMIN_PASSWORD,
-	DB_CLUSTER_NAME,
-	DB_COLLECTION,
-	PORT,
-} = process.env // импорт значений из .env
+  DB_ADMIN_NAME,
+  DB_ADMIN_PASSWORD,
+  DB_CLUSTER_NAME,
+  DB_COLLECTION,
+  PORT,
+} = process.env; // импорт значений из .env
 
-const DB_HOST_NEW = `mongodb+srv://${DB_ADMIN_NAME}:${DB_ADMIN_PASSWORD}@${DB_CLUSTER_NAME}.mongodb.net/${DB_COLLECTION}` // адрес для подключения к БД
-// console.log(DB_HOST_NEW)
+const DB_HOST_NEW = `mongodb+srv://${DB_ADMIN_NAME}:${DB_ADMIN_PASSWORD}@${DB_CLUSTER_NAME}.mongodb.net/${DB_COLLECTION}`; // адрес для подключения к БД
 
-const app = express() // создание веб-сервера
-
+const app = express(); // создание веб-сервера
 
 app.use(morgan("tiny")); // 'combined', 'common', 'short', 'tiny', 'dev'
 app.use(cors());
@@ -39,36 +34,31 @@ app.use(express.static("public"));
 
 app.use("/users", usersRoutes);
 
-app.use("/training", exerciseRoutes);
+app.use("/training", exerciseRouters);
 app.use("/diary", diaryRouters);
-app.use("/nutritions", nutritionsRouter);
-
-
-
-app.use('/products', productsRouter)
-app.use('/exercises', exerciseRoutes)
-
+app.use("/products", productsRouter);
+app.use("/exercises", exerciseRouters);
 
 app.use((_, res) => {
-	res.status(404).json({ message: 'Route not found' })
-})
+  res.status(404).json({ message: "Route not found" });
+});
 
 app.use((err, req, res, next) => {
-	const { status = 500, message = 'Server error' } = err
-	res.status(status).json({ message })
-})
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 mongoose //
-	.connect(DB_HOST_NEW)
-	.then(() => console.log('Database connection successful'.bold.italic.yellow))
-	.then(() =>
-		app.listen(PORT, () =>
-			console.log(
-				`Server is running. Use this API on port: ${PORT}`.bold.italic.yellow
-			)
-		)
-	)
-	.catch(err => {
-		console.error(err.message)
-		process.exit(1)
-	})
+  .connect(DB_HOST_NEW)
+  .then(() => console.log("Database connection successful".bold.italic.yellow))
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Server is running. Use this API on port: ${PORT}`.bold.italic.yellow
+      )
+    )
+  )
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
