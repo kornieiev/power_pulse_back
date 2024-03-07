c40740b2b9bd7167691688241b72a01e1eda9010
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-03-07 15:43:42 +0100

    add avatarURL

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,46 +1,18 @@
-// const { User } = require("../../models");
-// const path = require("path");
-// const fs = require("fs/promises");
-
-// // const Jimp = require("jimp");
-// const { changeImageSize } = require("../../helpers");
-// const cloudinaryFn = require("../../utils/cloudinaryFn");
-
-// const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
-// console.log("avatarsDir:", avatarsDir);
-
-// const updateAvatar = async (req, res, next) => {
-//   const { _id } = req.user;
-//   console.log("updateAvatar-_id", _id);
-
-//   // беремо тимчасовий шлях:
-//   const { path: tempUpload, originalname } = req.file;
-
-//   // // якщо якийсь юзер пришле файл з таким самим імям, як уже є - fs його перезапише. Тому до оригінального імені файлу додається id користувача:
-//   const filename = `${_id}_${originalname}`;
-
-//   // // створюємо шлях де він має бути:
-//   const resultUpload = path.join(avatarsDir, filename);
-
-//   // // переміщуємо файл:
-//   await fs.rename(tempUpload, resultUpload);
-
-//   // // записуємо новий шлях в базу:
-//   const avatarURL = path.join("avatars", filename);
-
-//   // // визизаємо Jimp для обрізання розміру фото:
-//   changeImageSize(avatarURL);
-
-//   await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
-
-//   await cloudinaryFn({ avatarURL });
-
-//   res.status(200).json({ avatarURL });
-// };
+const { User } = require("../../models");
 
 const updateAvatar = async (req, res) => {
+  const { _id: owner, email } = req.user;
+
   const avatarURL = req.file.path;
   console.log("updateAvatar-avatarURL", avatarURL);
+
+  await User.updateOne(
+    { owner },
+    {
+      $set: { avatarURL },
+    },
+    { new: true }
+  );
 
   res.status(200).json(avatarURL);
 };
--- models/index.js
+++ models/index.js
@@ -1,18 +1,17 @@
-const User = require('./user')
-const Metric = require('./metric')
-const DiaryProduct = require('./diaryProduct')
-const DiaryExercise = require('./diaryExercise')
-const Exercise = require('./exercise')
-const Product = require('./product')
-const Filter = require('./filter')
-// const Diary = require('./user')
+const User = require("./user");
+const Metric = require("./metric");
+const DiaryProduct = require("./diaryProduct");
+const DiaryExercise = require("./diaryExercise");
+const Exercise = require("./exercise");
+const Product = require("./product");
+const Filter = require("./filter");
 
 module.exports = {
-	User,
-	DiaryExercise,
-	DiaryProduct,
-	Metric,
-	Exercise,
-	Product,
-	Filter,
-}
+  User,
+  DiaryExercise,
+  DiaryProduct,
+  Metric,
+  Exercise,
+  Product,
+  Filter,
+};
