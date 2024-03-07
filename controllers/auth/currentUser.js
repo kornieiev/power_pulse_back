b4ced549dca861183512c2f34c85e8499c0f3513
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-03-07 11:53:05 +0100

    add metrics to currentUser

--- controllers/auth/currentUser.js
+++ controllers/auth/currentUser.js
@@ -1,4 +1,22 @@
+const { Metric } = require("../../models");
+const { User } = require("../../models");
+
 const currentUser = async (req, res, next) => {
-  res.status(200).json(req.user);
+  const { _id: owner, email } = req.user;
+
+  const userMetric = await Metric.find({ owner });
+  const userInfo = await User.find({ email });
+
+  await User.updateOne(
+    { owner },
+    {
+      $set: { metrics: userMetric[0] },
+    },
+    { new: true }
+  );
+
+  const user = [...userInfo, ...userMetric];
+
+  res.status(200).json(user);
 };
 module.exports = currentUser;
--- models/user.js
+++ models/user.js
@@ -36,6 +36,10 @@ const userSchema = new Schema(
       type: String,
       // required: true,
     },
+    metrics: {},
+    height: {
+      type: String,
+    },
   },
   {
     versionKey: false,
