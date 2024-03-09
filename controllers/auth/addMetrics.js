5ddf04f50a87735fba93a1544c08e5208287cec4
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2024-03-09 11:15:50 +0200

    add metrix

--- controllers/auth/addMetrics.js
+++ controllers/auth/addMetrics.js
@@ -1,51 +1,51 @@
-const BMR = require("../../helpers/BMR");
-const { Metric } = require("../../models");
-const { User } = require("../../models");
+const BMR = require('../../helpers/BMR')
+// const { Metric } = require("../../models");
+const { User } = require('../../models')
 
 const addMetrics = async (req, res, next) => {
-  const { _id: owner, email } = req.user;
-  console.log("===>>>owner", owner);
+	const { _id: owner, email } = req.user
+	console.log('===>>>owner', owner)
 
-  const {
-    height,
-    currentWeight,
-    desiredWeight,
-    blood,
-    sex,
-    levelActivity,
-    age,
-    userName,
-  } = req.body;
+	const {
+		height,
+		currentWeight,
+		desiredWeight,
+		blood,
+		sex,
+		levelActivity,
+		age,
+		userName,
+	} = req.body
 
-  const resultBMR = BMR(height, currentWeight, levelActivity, age, sex);
-  console.log("resultBMR", resultBMR);
+	const resultBMR = BMR(height, currentWeight, levelActivity, age, sex)
+	console.log('resultBMR', resultBMR)
 
-  // const userData = await User.findById(owner);
-  // console.log("userData", userData);
+	// const userData = await User.findById(owner);
+	// console.log("userData", userData);
 
-  const updatedUser = await User.findOneAndUpdate(
-    { email },
-    {
-      // ...userData,
-      $set: {
-        height,
-        currentWeight,
-        desiredWeight,
-        blood,
-        sex,
-        levelActivity,
-        age,
-        userName,
-        resultBMR,
-        userMetrics: true,
-      },
-    },
-    {
-      new: true,
-    }
-  );
+	const updatedUser = await User.findOneAndUpdate(
+		{ email },
+		{
+			// ...userData,
+			$set: {
+				height,
+				currentWeight,
+				desiredWeight,
+				blood,
+				sex,
+				levelActivity,
+				age,
+				userName,
+				resultBMR,
+				userMetrics: true,
+			},
+		},
+		{
+			new: true,
+		}
+	)
 
-  res.status(200).json({ updatedUser });
-};
+	res.status(200).json({ updatedUser })
+}
 
-module.exports = addMetrics;
+module.exports = addMetrics
