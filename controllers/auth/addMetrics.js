c7049fcb4e77ce1f75c387c9d829bfc343bbece5
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-03-10 19:38:20 +0100

    add metrics route in swagger

--- controllers/auth/addMetrics.js
+++ controllers/auth/addMetrics.js
@@ -45,7 +45,28 @@ const addMetrics = async (req, res, next) => {
     }
   );
 
-  res.status(200).json({ updatedUser });
+  console.log("updatedUser", updatedUser);
+
+  res.status(200).json({
+    updatedUser: {
+      email: updatedUser.email,
+      verify: updatedUser.verify,
+      userMetrics: updatedUser.userMetrics,
+      name: updatedUser.name,
+      createdAt: updatedUser.createdAt,
+      updatedAt: updatedUser.updatedAt,
+      birthday: updatedUser.birthday,
+      blood: updatedUser.blood,
+      currentWeight: updatedUser.currentWeight,
+      desiredWeight: updatedUser.desiredWeight,
+      height: updatedUser.height,
+      levelActivity: updatedUser.levelActivity,
+      resultBMR: updatedUser.resultBMR,
+      sex: updatedUser.sex,
+      userName: updatedUser.userName,
+      age: updatedUser.age,
+    },
+  });
 };
 
 module.exports = addMetrics;
