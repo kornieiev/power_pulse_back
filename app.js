2074a35824aaf7fa4af82806e0654d3d7b8492ad
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-03-10 15:02:27 +0100

    add swagger

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const usersRoutes = require("./routes/usersRoutes");
 const diaryRouters = require("./routes/diaryRoutes");
 require("dotenv").config(); // ищет в проекте файл .env и читает из него указанные в нем КЛЮЧ=значение
 require("colors"); // для подсвечивания информации выводимой в консоли
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const productsRouter = require("./routes/productsRouter");
 const exerciseRouters = require("./routes/exercisesRoutes");
@@ -18,18 +20,14 @@ const {
   PORT,
 } = process.env;
 
-
-const DB_HOST_NEW = `mongodb+srv://${DB_ADMIN_NAME}:${DB_ADMIN_PASSWORD}@${DB_CLUSTER_NAME}.mongodb.net/${DB_COLLECTION}` // адрес для подключения к БД
-
+const DB_HOST_NEW = `mongodb+srv://${DB_ADMIN_NAME}:${DB_ADMIN_PASSWORD}@${DB_CLUSTER_NAME}.mongodb.net/${DB_COLLECTION}`; // адрес для подключения к БД
 
 const app = express();
 
-
-app.use(morgan('tiny')) // 'combined', 'common', 'short', 'tiny', 'dev'
-app.use(cors())
-app.use(express.json())
-app.use(express.static('public'))
-
+app.use(morgan("tiny")); // 'combined', 'common', 'short', 'tiny', 'dev'
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/users", usersRoutes);
 
@@ -38,28 +36,28 @@ app.use("/diary", diaryRouters);
 app.use("/products", productsRouter);
 app.use("/exercises", exerciseRouters);
 
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((_, res) => {
-	res.status(404).json({ message: 'Route not found' })
-})
+  res.status(404).json({ message: "Route not found" });
+});
 
 app.use((err, req, res, next) => {
-	const { status = 500, message = 'Server error' } = err
-	res.status(status).json({ message })
-})
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 mongoose //
-	.connect(DB_HOST_NEW)
-	.then(() => console.log('Database connection successful'.bold.italic.yellow))
-	.then(() =>
-		app.listen(PORT, () =>
-			console.log(
-				`Server is running. Use this API on port: ${PORT}`.bold.italic.yellow
-			)
-		)
-	)
-	.catch(err => {
-		console.error(err.message)
-		process.exit(1)
-	})
+  .connect(DB_HOST_NEW)
+  .then(() => console.log("Database connection successful".bold.italic.yellow))
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Server is running. Use this API on port: ${PORT}`.bold.italic.yellow
+      )
+    )
+  )
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
