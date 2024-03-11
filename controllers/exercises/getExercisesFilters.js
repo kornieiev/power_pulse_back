78fe1801973f0af46e44e0c21edc9ed4e2674da4
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2024-03-11 23:59:22 +0200

    add exercises filter

--- controllers/exercises/getExercisesFilters.js
+++ controllers/exercises/getExercisesFilters.js
@@ -1,11 +1,11 @@
 const { Filter } = require('../../models')
 
 const getExercisesFilters = async (req, res) => {
-	const { page = 1, limit = 10 } = req.query
+	const { filter, page = 1, limit = 10 } = req.query
 
 	const skip = (page - 1) * limit
 
-	const data = await Filter.find({}, '', { skip, limit })
+	const data = await Filter.find({ filter }, '', { skip, limit })
 
 	res.status(200).json(data)
 }
