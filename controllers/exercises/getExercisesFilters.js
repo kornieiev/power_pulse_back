6bbd6b5ae7f3d39a9221524aee57f75d4a27be58
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-03-13 13:25:55 +0100

    fix getExercisesFilters

--- controllers/exercises/getExercisesFilters.js
+++ controllers/exercises/getExercisesFilters.js
@@ -1,13 +1,15 @@
-const { Filter } = require('../../models')
+const { Filter } = require("../../models");
 
 const getExercisesFilters = async (req, res) => {
-	const { filter, page = 1, limit = 10 } = req.query
+  // const { filter, page = 1, limit = 10 } = req.query
+  const { filter } = req.query;
 
-	const skip = (page - 1) * limit
+  // const skip = (page - 1) * limit
 
-	const data = await Filter.find({ filter }, '', { skip, limit })
+  // const data = await Filter.find({ filter }, '', { skip, limit })
+  const data = await Filter.find({ filter });
 
-	res.status(200).json(data)
-}
+  res.status(200).json(data);
+};
 
-module.exports = getExercisesFilters
+module.exports = getExercisesFilters;
