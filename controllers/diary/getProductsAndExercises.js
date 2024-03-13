e7d538c6598a7eeb46d5e9f4ad190d6d581e45ac
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2024-03-13 20:46:31 +0200

    add populate name and target

--- controllers/diary/getProductsAndExercises.js
+++ controllers/diary/getProductsAndExercises.js
@@ -1,30 +1,30 @@
-const { HttpError } = require("../../helpers");
-const { DiaryExercise } = require("../../models");
-const { DiaryProduct } = require("../../models");
+const { HttpError } = require('../../helpers')
+const { DiaryExercise } = require('../../models')
+const { DiaryProduct } = require('../../models')
 
 const getProductsAndExercises = async (req, res) => {
-  const { _id: owner } = req.user;
+	const { _id: owner } = req.user
 
-  const { date } = req.query;
+	const { date } = req.query
 
-  const [products, exercises] = await Promise.all([
-    DiaryProduct.find({ owner, date }).populate(
-      "productArr.productId",
-      "title category groupBloodNotAllowed"
-    ),
-    DiaryExercise.find({ owner, date }).populate(
-      "exerciseArr.exerciseId",
-      "bodyPart equipment"
-    ),
-  ]);
+	const [products, exercises] = await Promise.all([
+		DiaryProduct.find({ owner, date }).populate(
+			'productArr.productId',
+			'title category groupBloodNotAllowed'
+		),
+		DiaryExercise.find({ owner, date }).populate(
+			'exerciseArr.exerciseId',
+			'bodyPart equipment name target'
+		),
+	])
 
-  const data = [...products, ...exercises];
+	const data = [...products, ...exercises]
 
-  if (data.length < 1) {
-    throw HttpError(404, "Information by your request not found");
-  }
+	if (data.length < 1) {
+		throw HttpError(404, 'Information by your request not found')
+	}
 
-  res.status(200).json(data);
-};
+	res.status(200).json(data)
+}
 
-module.exports = getProductsAndExercises;
+module.exports = getProductsAndExercises
