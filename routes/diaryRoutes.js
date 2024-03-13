7a51dc942483f276d5372c7b1bdcc598c6995895
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2024-03-13 20:38:43 +0200

    delete fix

--- routes/diaryRoutes.js
+++ routes/diaryRoutes.js
@@ -1,58 +1,53 @@
-const express = require("express");
-const diaryRouter = express.Router();
+const express = require('express')
+const diaryRouter = express.Router()
 
-const { authenticate, isValidId } = require("../middlewares");
+const { authenticate, isValidId } = require('../middlewares')
 const {
-  deleteUserExercises,
-  addUserExercises,
-  addUserProducts,
-  deleteUserProducts,
-  getProductsAndExercises,
-  getCommonDataByDate,
-} = require("../controllers/diary");
-const { validateBody } = require("../helpers");
+	deleteUserExercises,
+	addUserExercises,
+	addUserProducts,
+	deleteUserProducts,
+	getProductsAndExercises,
+	getCommonDataByDate,
+} = require('../controllers/diary')
+const { validateBody } = require('../helpers')
 
 const {
-  UserProductSchema,
-  UserExercisesSchema,
-} = require("../schemas/diarySchemas");
+	UserProductSchema,
+	UserExercisesSchema,
+} = require('../schemas/diarySchemas')
 
 // add product
 diaryRouter.post(
-  "/products",
-  authenticate,
-  validateBody(UserProductSchema),
-  addUserProducts
-);
+	'/products',
+	authenticate,
+	validateBody(UserProductSchema),
+	addUserProducts
+)
 
 // add exercises
 diaryRouter.post(
-  "/exercises",
-  authenticate,
-  validateBody(UserExercisesSchema),
-  addUserExercises
-);
+	'/exercises',
+	authenticate,
+	validateBody(UserExercisesSchema),
+	addUserExercises
+)
 
 // get product and exercises
-diaryRouter.get("/", authenticate, getProductsAndExercises);
+diaryRouter.get('/', authenticate, getProductsAndExercises)
 
 // get common data by Date - query
-diaryRouter.get("/common/:date", authenticate, getCommonDataByDate);
+// diaryRouter.get('/common/:date', authenticate, getCommonDataByDate)
 
 // delete products
-diaryRouter.delete(
-  "/products/:id:date",
-  authenticate,
-  isValidId,
-  deleteUserProducts
-);
+diaryRouter.delete('/products/:id', authenticate, isValidId, deleteUserProducts)
 
 // delete exercises
 diaryRouter.delete(
-  "/exercises/:id",
-  authenticate,
-  isValidId,
-  deleteUserExercises
-);
+	'/exercises/:id',
+	authenticate,
+	isValidId,
+	deleteUserExercises
+)
 
-module.exports = diaryRouter;
+module.exports = diaryRouter
