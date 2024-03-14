37f881fe41f8399b8e684725fdef0d7e2d8abfdc
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-03-14 17:26:28 +0100

    change email content

--- helpers/emailContentMaker.js
+++ helpers/emailContentMaker.js
@@ -1,15 +1,14 @@
-require("dotenv").config();
-const { BASE_URL } = process.env;
+// require("dotenv").config();
+// const { BASE_URL } = process.env;
 
-const emailContentMaker = (verificationCode) => {
+const emailContentMaker = (email, name) => {
   console.log(">>>>>>>>>> emailContentMaker working...");
   return `
   <div style="background-color: #f0f0f0; color: #333; padding: 20px; font-family: Arial, sans-serif;">
-    <a target='_blank' href='https://powerpulseback.onrender.com/users/verify/${verificationCode}'>
-      Click verify email</a>
-    <p style="font-size: 16px; margin-bottom: 10px;">Your verificationToken: ${verificationCode}</p>
-    <p>Copy it and use in Postman, until we didn't create another method)</p>
-    </div>
+    <p style="font-size: 16px; margin-bottom: 10px;">Dear ${name}</p>
+    <p>We are glad that you have chosen our service as a tool to reduce the amount of fat in your body 🐖</p>
+    <p>Have a good day and work harder!</p>
+  </div>
     `;
 };
 
